c01ad1952b0e6c6e1b589e6b6ee9d07d343b9e8b
galind <guillem.yt1@gmail.com>
2023-04-27 19:52:59 +0200

    Updated customer check function

--- cogs/requests.py
+++ cogs/requests.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord import app_commands
+from typing import Optional
 import discord
 
 from bot import Bot
@@ -11,44 +12,44 @@ class Requests(commands.GroupCog, group_name='requests'):
         self.bot = bot
         self.not_customer = 'Customer has not been onboarded yet'
 
-    async def customer_exists(self, channel_id: int) -> bool:
+    async def customer_exists(self, channel_id: int) -> Optional[dict]:
         customer = mongo.Customer(channel_id)
         customer_info = await customer.check()
-        return bool(customer_info)
+        return customer_info
 
     @app_commands.command()
     async def board(self, interaction: discord.Interaction) -> None:
         """Check the status of your requests board"""
-        result = await self.customer_exists(interaction.channel_id)
+        customer_info = await self.customer_exists(interaction.channel_id)
 
-        if result is False:
+        if bool(customer_info) is False:
             await interaction.response.send_message(self.not_customer)
             return
 
     @app_commands.command()
     async def add(self, interaction: discord.Interaction) -> None:
         """Add a new request to the board"""
-        result = await self.customer_exists(interaction.channel_id)
+        customer_info = await self.customer_exists(interaction.channel_id)
 
-        if result is False:
+        if bool(customer_info) is False:
             await interaction.response.send_message(self.not_customer)
             return
 
     @app_commands.command()
     async def update(self, interaction: discord.Interaction) -> None:
         """Update an existing request from the board"""
-        result = await self.customer_exists(interaction.channel_id)
+        customer_info = await self.customer_exists(interaction.channel_id)
 
-        if result is False:
+        if bool(customer_info) is False:
             await interaction.response.send_message(self.not_customer)
             return
 
     @app_commands.command()
     async def remove(self, interaction: discord.Interaction) -> None:
         """Remove an existing request from the board"""
-        result = await self.customer_exists(interaction.channel_id)
+        customer_info = await self.customer_exists(interaction.channel_id)
 
-        if result is False:
+        if bool(customer_info) is False:
             await interaction.response.send_message(self.not_customer)
             return
 
