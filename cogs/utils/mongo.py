6235f815b09277f9f545a67749eb6f31b01558c1
galind <guillem.yt1@gmail.com>
2023-04-27 14:55:27 +0200

    Updated from client to customer

--- cogs/utils/mongo.py
+++ cogs/utils/mongo.py
@@ -5,17 +5,17 @@ client = motor.AsyncIOMotorClient('mongodb://localhost:27017')
 db = client['requests-manager']
 
 
-class Client:
+class Customer:
     def __init__(self, channel: int = None) -> None:
-        self.clients = db['clients']
+        self.customer = db['customer']
         self.query = {'_id': channel}
 
     async def create(self, query: dict) -> None:
-        await self.clients.insert_one(query)
+        await self.customer.insert_one(query)
 
     async def check(self) -> Optional[dict]:
-        return await self.clients.find_one(self.query)
+        return await self.customer.find_one(self.query)
 
     async def update(self, query: dict) -> None:
         set_query = {'$set': query}
-        await self.clients.update_one(self.query, set_query, upsert=True)
+        await self.customer.update_one(self.query, set_query, upsert=True)
