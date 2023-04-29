708c9757dc509b5dcaf784c8e28792bbc70fb88a
galind <guillem.yt1@gmail.com>
2023-04-29 12:14:55 +0200

    Added embed and view structure for update and delete

--- cogs/requests.py
+++ cogs/requests.py
@@ -14,6 +14,21 @@ def simple_request_embed(info: dict) -> discord.Embed:
     return embed
 
 
+def view_requests_embed(requests: list) -> discord.Embed:
+    embed = discord.Embed(title='Feature requests')
+    priority_list = ['🔴', '🟡', '🟢']
+
+    count = 1
+    for request in requests:
+        name = request.get('name')
+        priority = request.get('priority')
+        value = f'Priority: {priority_list[priority]}'
+        embed.add_field(name=f'{count}. {name}', value=value, inline=False)
+        count += 1
+
+    return embed
+
+
 class AddPriorityView(discord.ui.View):
     def __init__(self, info: dict) -> None:
         super().__init__(timeout=300)
@@ -21,10 +36,7 @@ class AddPriorityView(discord.ui.View):
         self.board_command = '</requests board:1101120797424234587>'
 
     async def on_timeout(self) -> None:
-        for item in self.children:
-            item.disabled = True
-
-        await self.message.edit(view=self)
+        await self.message.delete()
 
     async def add_request(self) -> None:
         self.request_info['priority'] = self.priority
@@ -46,7 +58,7 @@ class AddPriorityView(discord.ui.View):
         )
 
     @discord.ui.button(
-        label='High priority', style=discord.ButtonStyle.primary
+        label='High', emoji='🔴', style=discord.ButtonStyle.secondary
     )
     async def high_priority(
         self, interaction: discord.Interaction, button: discord.Button
@@ -56,7 +68,7 @@ class AddPriorityView(discord.ui.View):
         await self.add_request()
 
     @discord.ui.button(
-        label='Medium priority', style=discord.ButtonStyle.primary
+        label='Medium', emoji='🟡', style=discord.ButtonStyle.secondary
     )
     async def medium_priority(
         self, interaction: discord.Interaction, button: discord.Button
@@ -66,7 +78,7 @@ class AddPriorityView(discord.ui.View):
         await self.add_request()
 
     @discord.ui.button(
-        label='Low priority', style=discord.ButtonStyle.primary
+        label='Low', emoji='🟢', style=discord.ButtonStyle.secondary
     )
     async def low_priority(
         self, interaction: discord.Interaction, button: discord.Button
@@ -105,15 +117,33 @@ class AddRequestModal(discord.ui.Modal):
         await interaction.response.defer()
 
 
+class SelectRequestButton(discord.ui.Button):
+    def __init__(self, index: int, action: str) -> None:
+        self.action = action
+
+        if self.action == 'update':
+            style = discord.ButtonStyle.primary
+        else:
+            style = discord.ButtonStyle.danger
+
+        super().__init__(label=index, style=style)
+
+
 class SelectRequestView(discord.ui.View):
-    def __init__(self, action: str) -> None:
+    def __init__(self, requests: list, action: str) -> None:
         super().__init__(timeout=600)
+        for i in range(len(requests)):
+            self.add_item(SelectRequestButton(i+1, action))
+
+    async def on_timeout(self) -> None:
+        await self.message.delete()
 
 
 class Requests(commands.GroupCog, group_name='requests'):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.not_customer = 'Customer has not been onboarded yet'
+        self.no_requests = 'You don\'t have any request on the board'
 
     async def customer_exists(self, channel_id: int) -> Optional[dict]:
         customer = mongo.Customer(channel_id)
@@ -150,14 +180,50 @@ class Requests(commands.GroupCog, group_name='requests'):
             await interaction.response.send_message(self.not_customer)
             return
 
+        requests = customer_info.get('requests', [])
+        if len(requests) == 0:
+            await interaction.response.send_message(self.no_requests)
+            return
+
+        sublists = []
+        sorted_requests = sorted(requests, key=lambda r: r.get('priority'))
+        for i in range(0, len(requests), 25):
+            sublist = sorted_requests[i:i+25]
+            sublists.append(sublist)
+
+        for request_list in sublists:
+            embed = view_requests_embed(request_list)
+            view = SelectRequestView(request_list, 'update')
+            view.message = await interaction.channel.send(
+                embed=embed, view=view
+            )
+
     @app_commands.command()
-    async def remove(self, interaction: discord.Interaction) -> None:
-        """Remove an existing request from the board"""
+    async def delete(self, interaction: discord.Interaction) -> None:
+        """Delete an existing request from the board"""
         customer_info = await self.customer_exists(interaction.channel_id)
 
         if bool(customer_info) is False:
             await interaction.response.send_message(self.not_customer)
             return
+
+        requests = customer_info.get('requests', [])
+        if len(requests) == 0:
+            await interaction.response.send_message(self.no_requests)
+            return
+
+        sublists = []
+        sorted_requests = sorted(requests, key=lambda r: r.get('priority'))
+        for i in range(0, len(requests), 25):
+            sublist = sorted_requests[i:i+25]
+            sublists.append(sublist)
+
+        for request_list in sublists:
+            embed = view_requests_embed(request_list)
+            view = SelectRequestView(request_list, 'delete')
+            view.message = await interaction.channel.send(
+                embed=embed, view=view
+            )
 
 
 async def setup(bot: Bot) -> None:
