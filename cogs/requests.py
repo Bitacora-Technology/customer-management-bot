9a37760f67179ae3d11c7e2cf61c9562e5d699d1
galind <guillem.yt1@gmail.com>
2023-04-29 15:00:30 +0200

    Added update and delete request view structure

--- cogs/requests.py
+++ cogs/requests.py
@@ -24,7 +24,7 @@ def view_requests_embed(requests: list) -> discord.Embed:
         priority = request.get('priority')
         value = f'Priority: {priority_list[priority]}'
         embed.add_field(name=f'{count}. {name}', value=value, inline=False)
-        count += 1
+        count = 1
 
     return embed
 
@@ -117,16 +117,92 @@ class AddRequestModal(discord.ui.Modal):
         await interaction.response.defer()
 
 
+class UpdateRequestView(discord.ui.View):
+    def __init__(self, info: dict) -> None:
+        super().__init__(timeout=600)
+        self.request_info = info
+
+    @discord.ui.button(label='Information', style=discord.ButtonStyle.primary)
+    async def information(
+        self, interaction: discord.Interaction, button: discord.Button
+    ) -> None:
+        pass
+
+    @discord.ui.button(label='Priority', style=discord.ButtonStyle.primary)
+    async def priority(
+        self, interaction: discord.Interaction, button: discord.Button
+    ) -> None:
+        pass
+
+    @discord.ui.button(label='Go back', style=discord.ButtonStyle.secondary)
+    async def go_back(
+        self, interaction: discord.Interaction, button: discord.Button
+    ) -> None:
+        pass
+
+
+class DeleteRequestView(discord.ui.View):
+    def __init__(self, info: dict) -> None:
+        super().__init__(timeout=600)
+        self.request_info = info
+
+    @discord.ui.button(
+        label='Yes, I want to delete it',
+        style=discord.ButtonStyle.danger
+    )
+    async def delete(
+        self, interaction: discord.Interaction, button: discord.Button
+    ) -> None:
+        pass
+
+    @discord.ui.button(label='Go back', style=discord.ButtonStyle.secondary)
+    async def go_back(
+        self, interaction: discord.Interaction, button: discord.Button
+    ) -> None:
+        pass
+
+
 class SelectRequestButton(discord.ui.Button):
     def __init__(self, index: int, action: str) -> None:
         self.action = action
-
         if self.action == 'update':
             style = discord.ButtonStyle.primary
         else:
             style = discord.ButtonStyle.danger
-
         super().__init__(label=index, style=style)
+        self.not_found = 'The request was not found on the board'
+
+    async def find_request(self, message: discord.Message) -> Optional[dict]:
+        field = message.embeds[0].fields[self.label-1]
+        name = ' '.join(field.name.split(' ')[1:])
+
+        customer = mongo.Customer(message.channel.id)
+        customer_info = await customer.check()
+
+        requests = customer_info.get('requests', [])
+        for request in requests:
+            if request.get('name') == name:
+                return request
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        request_info = await self.find_request(interaction.message)
+
+        if request_info is None:
+            await interaction.response.send_message(self.not_found)
+            return
+
+        embed = simple_request_embed(request_info)
+        if self.action == 'update':
+            embed.set_footer(
+                text='Select what you want to update'
+            )
+            view = UpdateRequestView(request_info)
+        else:
+            embed.set_footer(
+                text='Once you have confirmed it cannot be restored'
+            )
+            view = DeleteRequestView(request_info)
+        await interaction.response.edit_message(embed=embed, view=view)
 
 
 class SelectRequestView(discord.ui.View):
@@ -143,7 +219,7 @@ class Requests(commands.GroupCog, group_name='requests'):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.not_customer = 'Customer has not been onboarded yet'
-        self.no_requests = 'You don\'t have any request on the board'
+        self.no_requests = 'You do not have any request on the board'
 
     async def customer_exists(self, channel_id: int) -> Optional[dict]:
         customer = mongo.Customer(channel_id)
@@ -171,9 +247,9 @@ class Requests(commands.GroupCog, group_name='requests'):
         modal = AddRequestModal()
         await interaction.response.send_modal(modal)
 
-    @app_commands.command()
-    async def update(self, interaction: discord.Interaction) -> None:
-        """Update an existing request from the board"""
+    async def send_board(
+        self, interaction: discord.Interaction, action: str
+    ) -> None:
         customer_info = await self.customer_exists(interaction.channel_id)
 
         if bool(customer_info) is False:
@@ -185,6 +261,8 @@ class Requests(commands.GroupCog, group_name='requests'):
             await interaction.response.send_message(self.no_requests)
             return
 
+        await interaction.response.send_message('\u200b', delete_after=0)
+
         sublists = []
         sorted_requests = sorted(requests, key=lambda r: r.get('priority'))
         for i in range(0, len(requests), 25):
@@ -193,37 +271,20 @@ class Requests(commands.GroupCog, group_name='requests'):
 
         for request_list in sublists:
             embed = view_requests_embed(request_list)
-            view = SelectRequestView(request_list, 'update')
+            view = SelectRequestView(request_list, action)
             view.message = await interaction.channel.send(
                 embed=embed, view=view
             )
+
+    @app_commands.command()
+    async def update(self, interaction: discord.Interaction) -> None:
+        """Update an existing request from the board"""
+        await self.send_board(interaction, 'update')
 
     @app_commands.command()
     async def delete(self, interaction: discord.Interaction) -> None:
         """Delete an existing request from the board"""
-        customer_info = await self.customer_exists(interaction.channel_id)
-
-        if bool(customer_info) is False:
-            await interaction.response.send_message(self.not_customer)
-            return
-
-        requests = customer_info.get('requests', [])
-        if len(requests) == 0:
-            await interaction.response.send_message(self.no_requests)
-            return
-
-        sublists = []
-        sorted_requests = sorted(requests, key=lambda r: r.get('priority'))
-        for i in range(0, len(requests), 25):
-            sublist = sorted_requests[i:i+25]
-            sublists.append(sublist)
-
-        for request_list in sublists:
-            embed = view_requests_embed(request_list)
-            view = SelectRequestView(request_list, 'delete')
-            view.message = await interaction.channel.send(
-                embed=embed, view=view
-            )
+        await self.send_board(interaction, 'delete')
 
 
 async def setup(bot: Bot) -> None:
