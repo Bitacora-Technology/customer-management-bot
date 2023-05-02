60f120060e64a7c442157702b79a044ae03c1591
galind <guillem.yt1@gmail.com>
2023-05-02 22:13:51 +0200

    Updated logs to error only

--- launcher.py
+++ launcher.py
@@ -32,11 +32,11 @@ def setup_logging():
     try:
         discord.utils.setup_logging()
         max_bytes = 32 * 1024 * 1024  # 32 MiB
-        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord').setLevel(logging.ERROR)
         logging.getLogger('discord.http').setLevel(logging.WARNING)
         logging.getLogger('discord.state').addFilter(RemoveNoise())
 
-        log.setLevel(logging.INFO)
+        log.setLevel(logging.ERROR)
         handler = RotatingFileHandler(
             filename='discord.log',
             encoding='utf-8',
